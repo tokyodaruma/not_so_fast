1e8400037c75faba79a2a000b3a57c8aa283eec3
Zach Carter <zacarter93@gmail.com>
2022-08-06 17:59:23 +0900

    added native api for index, update, and create notifications

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :photo
+  acts_as_token_authenticatable
 end
--- app/policies/notification_policy.rb
+++ app/policies/notification_policy.rb
@@ -1,8 +1,16 @@
 class NotificationPolicy < ApplicationPolicy
   class Scope < Scope
-    # NOTE: Be explicit about which records you allow access to!
-    # def resolve
-    #   scope.all
-    # end
+    def resolve
+      scope.all
+    end
+  end
+
+  def update?
+    record.user == user
+  end
+
+  def create?
+    puts "HEY I'M HERE/n/n/n/n/n/n/n/n/n/n/n/n/n/n/n/n/n/n/n/n/n/n/n/n/n/n/n/n/n/n/n/n/n/n/n/n/n/n/n/n/n/n/n/n"
+    true
   end
 end
