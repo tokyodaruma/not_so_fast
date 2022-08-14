9010723d71c8b14a382080a1764ac32bc7add8c4
Zach Carter <zacarter93@gmail.com>
2022-08-14 20:49:29 +0900

    added default avatar on sign-in

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
   resources :sites
-  resources :notifications, only: [:index, :show] do
+  resources :notifications, only: %i[index show] do
     resources :sites
   end
 
