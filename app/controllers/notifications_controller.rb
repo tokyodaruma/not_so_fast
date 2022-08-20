c00ea75fe16e69e690566522fd83cc109a0747f0
Satoka Sotome <satoka121@gmail.com>
2022-08-20 11:05:07 +0900

    moved the create new site button from the sites#index to notifications#index

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,5 +1,6 @@
 class NotificationsController < ApplicationController
   def index
     @notifications = policy_scope(Notification).where(read:false).order(accessed_at: :desc)
+    @site = Site.new
   end
 end
